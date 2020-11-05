8bdfcc1587c91e58ed9a7ab710364b02bb06766f
洪洋 陈 <chenhongyang0714@outlook.com>
2020-11-05 23:39:41 +0800

    1.添加 Junit 测试单元

--- 02-di/src/test/java/MyTest.java
+++ 02-di/src/test/java/MyTest.java
@@ -1,9 +1,18 @@
 import com.study.pojo.Student;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.toString());
+    }
+
+    // 添加 Junit 测试单元
+    @Test
+    public void test2(){
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
--- first/src/test/java/MyTest.java
+++ first/src/test/java/MyTest.java
@@ -7,7 +7,11 @@ public class MyTest {
         // 获取Spring上下文对象
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         // 我们的对象现在都在Spring中管理了，我们要使用的时候  可以直接去里面取出来
-        Hello hello = (Hello) context.getBean("hello");
+//        Hello hello = (Hello) context.getBean("hello");
+//        System.out.println(hello.toString());
+
+        // 该方式不用强制类型转换
+        Hello hello = context.getBean("hello", Hello.class);
         System.out.println(hello.toString());
     }
 }
